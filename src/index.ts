37f16ad89480cd16cfda3f11af1a4ffcdb23fce7
Lucas Ferreira Simão <22101918@aluno.cotemig.com.br>
2023-10-26 23:37:51 +0000

    feat: add mongoCLient with mongodb connection

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,22 @@
 import express from "express";
 import { GetUsersController } from "./controllers/get-users/get-users";
+import { MongoClient } from "./database/mongo";
 import { MongoGetUsersRepository } from "./repositories/get-users/mongo-get-users";
 
-const app = express();
+const main = async () => {
+    const app = express();
+    const port = 8000;
 
-const port = 8000;
+    await MongoClient.connect();
 
-app.get("/users", async (req, res) => {
-    const mongoGetUsersRepository = new MongoGetUsersRepository();
-    const getUsersController = new GetUsersController(mongoGetUsersRepository);
-    const { statusCode, body } = await getUsersController.handle();
+    app.get("/users", async (req, res) => {
+        const mongoGetUsersRepository = new MongoGetUsersRepository();
+        const getUsersController = new GetUsersController(mongoGetUsersRepository);
+        const { statusCode, body } = await getUsersController.handle();
 
-    res.status(statusCode).send(body);
-});
+        res.status(statusCode).send(body);
+    });
 
-app.listen(port);
+    app.listen(port);
+};
+main();
