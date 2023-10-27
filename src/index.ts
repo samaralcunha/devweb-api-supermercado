36b2b00328928740612fc32b6139f5cb81bc67f7
Lucas Ferreira Simão <22101918@aluno.cotemig.com.br>
2023-10-27 19:06:24 +0000

    feat: add DeleteUserController

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 import express from 'express';
 import { CreateUserController } from './controllers/create-user/create-user';
+import { DeleteUserController } from './controllers/delete-user/delete-user';
 import { GetUsersController } from './controllers/get-users/get-users';
 import { UpdateUserController } from './controllers/update-user/update-user';
 import { MongoClient } from './database/mongo';
 import { MongoCreateUserRepository } from './repositories/create-user/mongo-create-user';
+import { MongoDeleteUserRepository } from './repositories/delete-user/mongo-delete-user';
 import { MongoGetUsersRepository } from './repositories/get-users/mongo-get-users';
 import { MongoUpdateUserRepository } from './repositories/update-user/mongo-update-user';
 
@@ -33,10 +35,19 @@ const main = async () => {
     });
 
     app.put('/users/:id', async (req, res) => {
-        const mongoUpdateUsersRepository = new MongoUpdateUserRepository();
-        const createUserController = new UpdateUserController(mongoUpdateUsersRepository);
-        const { statusCode, body } = await createUserController.handle({
+        const mongoUpdateUserRepository = new MongoUpdateUserRepository();
+        const updateUserController = new UpdateUserController(mongoUpdateUserRepository);
+        const { statusCode, body } = await updateUserController.handle({
             body: req.body,
+            params: req.params,
+        });
+        res.status(statusCode).send(body);
+    });
+
+    app.delete('/users/:id', async (req, res) => {
+        const mongoDeleteUserRepository = new MongoDeleteUserRepository();
+        const deleteUserController = new DeleteUserController(mongoDeleteUserRepository);
+        const { statusCode, body } = await deleteUserController.handle({
             params: req.params,
         });
         res.status(statusCode).send(body);
